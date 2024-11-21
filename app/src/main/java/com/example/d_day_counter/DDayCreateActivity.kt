4bebd5eb11622b2272e214755a479618474c286f
Lsunae <sjsao5012@gmail.com>
2024-11-21 23:53:01 +0900

    Feat: DatePickerDialog 적용

--- app/src/main/java/com/example/d_day_counter/DDayCreateActivity.kt
+++ app/src/main/java/com/example/d_day_counter/DDayCreateActivity.kt
@@ -1,8 +1,11 @@
 package com.example.d_day_counter
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.d_day_counter.databinding.ActivityDDayCreateBinding
+import java.util.Calendar
 
 class DDayCreateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDDayCreateBinding
@@ -19,9 +22,29 @@ class DDayCreateActivity : AppCompatActivity() {
         clickListener()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun clickListener() {
         binding.apply {
             ivClose.setOnClickListener { finish() }
+
+            val cal = Calendar.getInstance()
+            val year = cal.get(Calendar.YEAR)
+            val month = cal.get(Calendar.MONTH)
+            val day = cal.get(Calendar.DAY_OF_MONTH)
+            tvDate.text = "${year}년 ${month}월 ${day}일"
+            llDate.setOnClickListener {
+                val datePickerDialog = DatePickerDialog(
+                    this@DDayCreateActivity,
+                    R.style.SpinnerDatePicker,
+                    { _, year, month, day ->
+                        tvDate.text = "${year}년 ${month}월 ${day}일"
+                    },
+                    year,
+                    month,
+                    day
+                )
+                datePickerDialog.show()
+            }
         }
     }
 }
