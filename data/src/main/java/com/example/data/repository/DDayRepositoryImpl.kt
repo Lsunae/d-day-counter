fbb1fb73eb7eece06aa738b3f9b8efe105f6c702
Lsunae <sjsao5012@gmail.com>
2025-04-08 18:09:34 +0900

    Feat: DDayRepositoryImpl 구현

--- data/src/main/java/com/example/data/repository/DDayRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/DDayRepositoryImpl.kt
@@ -1,4 +1,15 @@
 package com.example.data.repository
 
-class DDayRepositoryImpl {
+import com.example.domain.model.DDay
+import com.example.domain.repository.DDayRepository
+import com.example.domain.source.DDayLocalDataSource
+import javax.inject.Inject
+
+class DDayRepositoryImpl @Inject constructor(private val localDataSource: DDayLocalDataSource) :
+    DDayRepository {
+    override suspend fun getAllDDays(): List<DDay> = localDataSource.getAllDDays()
+
+    override suspend fun insertDDay(dDay: DDay) = localDataSource.insertDDay(dDay)
+
+    override suspend fun deleteDDay(dDay: DDay) = localDataSource.deleteDDay(dDay)
 }
