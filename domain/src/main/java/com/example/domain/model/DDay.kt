c3461f41abe3de66a28810ddd24033346027a9d6
Lsunae <sjsao5012@gmail.com>
2025-04-08 18:05:57 +0900

    Feat: DDay 데이터 모델 생성 및 DDayLocalDataSource 구현

---
+++ domain/src/main/java/com/example/domain/model/DDay.kt
@@ -0,0 +1,7 @@
+package com.example.domain.model
+
+data class DDay(
+    val id: Int,
+    val title: String,
+    val date: Long
+)
--- domain/src/main/java/com/example/domain/source/DDayLocalDataSource.kt
+++ domain/src/main/java/com/example/domain/source/DDayLocalDataSource.kt
@@ -1,4 +1,9 @@
 package com.example.domain.source
 
+import com.example.domain.model.DDay
+
 interface DDayLocalDataSource {
+    suspend fun getAllDDays(): List<DDay>
+    suspend fun insertDDay(dDay: DDay)
+    suspend fun deleteDDay(dDay: DDay)
 }
