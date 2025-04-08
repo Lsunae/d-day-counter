1374b04be5715e312f0c5918bd64c9409918bc70
Lsunae <sjsao5012@gmail.com>
2025-04-08 18:09:12 +0900

    Feat: DDayRepository interface 구현

--- domain/src/main/java/com/example/domain/repository/DDayRepository.kt
+++ domain/src/main/java/com/example/domain/repository/DDayRepository.kt
@@ -1,4 +1,9 @@
 package com.example.domain.repository
 
+import com.example.domain.model.DDay
+
 interface DDayRepository {
+    suspend fun getAllDDays(): List<DDay>
+    suspend fun insertDDay(dDay: DDay)
+    suspend fun deleteDDay(dDay: DDay)
 }
