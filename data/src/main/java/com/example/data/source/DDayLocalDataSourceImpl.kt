914e9bbc66c8f73e86e65cddc67f6362ddfe3aad
Lsunae <sjsao5012@gmail.com>
2025-04-08 18:08:34 +0900

    Feat: DDayLocalDataSourceImpl 구현

--- data/src/main/java/com/example/data/source/DDayLocalDataSourceImpl.kt
+++ data/src/main/java/com/example/data/source/DDayLocalDataSourceImpl.kt
@@ -1,4 +1,17 @@
 package com.example.data.source
 
-class DDayLocalDataSourceImpl {
+import com.example.data.dao.DDayDao
+import com.example.data.mapper.toDomain
+import com.example.data.mapper.toEntity
+import com.example.domain.model.DDay
+import com.example.domain.source.DDayLocalDataSource
+import javax.inject.Inject
+
+class DDayLocalDataSourceImpl @Inject constructor(private val dDayDao: DDayDao) :
+    DDayLocalDataSource {
+    override suspend fun getAllDDays(): List<DDay> = dDayDao.getAllDDays().map { it.toDomain() }
+
+    override suspend fun insertDDay(dDay: DDay) = dDayDao.insertDDay(dDay.toEntity())
+
+    override suspend fun deleteDDay(dDay: DDay) = dDayDao.deleteDDay(dDay.toEntity())
 }
