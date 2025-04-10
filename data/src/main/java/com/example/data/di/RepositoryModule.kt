56590fd26f92ec5d11df6af532fa74871299eb90
Lsunae <sjsao5012@gmail.com>
2025-04-10 12:33:32 +0900

    Feat: RepositoryModule 구현

--- data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,4 +1,18 @@
 package com.example.data.di
 
+import com.example.data.repository.DDayRepositoryImpl
+import com.example.domain.repository.DDayRepository
+import com.example.domain.source.DDayLocalDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 class RepositoryModule {
+    @Provides
+    fun provideDDayRepository(localDataSource: DDayLocalDataSource): DDayRepository {
+        return DDayRepositoryImpl(localDataSource)
+    }
 }
