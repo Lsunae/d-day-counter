99e7baf4274b7607a25712001a9e1023a3e3d4d4
Lsunae <sjsao5012@gmail.com>
2025-04-29 16:40:40 +0900

    Feat: ViewModel 구현

--- presentation/src/main/java/com/example/presentation/viewmodel/DDayViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/DDayViewModel.kt
@@ -1,16 +1,43 @@
 package com.example.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.model.DDay
 import com.example.domain.usecase.AddDDayUseCase
 import com.example.domain.usecase.DeleteDDayUseCase
 import com.example.domain.usecase.GetAllDDaysUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DDayViewModel @Inject constructor(
-    getAllDDaysUseCase: GetAllDDaysUseCase,
-    addDDayUseCase: AddDDayUseCase,
-    deleteDDayUseCase: DeleteDDayUseCase
-) {
+    private val getAllDDaysUseCase: GetAllDDaysUseCase,
+    private val addDDayUseCase: AddDDayUseCase,
+    private val deleteDDayUseCase: DeleteDDayUseCase
+) : ViewModel() {
+    private val _dDays = MutableLiveData<List<DDay>>()
+    val dDays: LiveData<List<DDay>> = _dDays
 
+    fun getDDays() {
+        viewModelScope.launch {
+            _dDays.value = getAllDDaysUseCase()
+        }
+    }
+
+    fun addDDay(dDay: DDay) {
+        viewModelScope.launch {
+            addDDayUseCase(dDay)
+            getDDays()
+        }
+    }
+
+    fun deleteDDay(dDay: DDay) {
+        viewModelScope.launch {
+            deleteDDayUseCase(dDay)
+            getDDays()
+        }
+    }
 }
