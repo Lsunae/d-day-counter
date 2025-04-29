71bf304a295d09a14a23572ab41f737d422feae7
Lsunae <sjsao5012@gmail.com>
2025-04-29 10:43:41 +0900

    Refactor: 의존성 주입 모듈 어노테이션 추가

--- data/src/main/java/com/example/data/di/DataSourceModule.kt
+++ data/src/main/java/com/example/data/di/DataSourceModule.kt
@@ -3,9 +3,14 @@ package com.example.data.di
 import com.example.data.dao.DDayDao
 import com.example.data.source.DDayLocalDataSourceImpl
 import com.example.domain.source.DDayLocalDataSource
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 class DataSourceModule {
     @Provides
     @Singleton
