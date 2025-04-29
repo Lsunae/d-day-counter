ba328d9bb35cc27b57bf33154cf788910cd37fd6
Lsunae <sjsao5012@gmail.com>
2025-04-29 10:36:56 +0900

    Feat: DataSourceModule 구현

--- data/src/main/java/com/example/data/di/DataSourceModule.kt
+++ data/src/main/java/com/example/data/di/DataSourceModule.kt
@@ -1,4 +1,14 @@
 package com.example.data.di
 
+import com.example.data.dao.DDayDao
+import com.example.data.source.DDayLocalDataSourceImpl
+import com.example.domain.source.DDayLocalDataSource
+import dagger.Provides
+import javax.inject.Singleton
+
 class DataSourceModule {
+    @Provides
+    @Singleton
+    fun provideDDayDatasource(dDayDao: DDayDao): DDayLocalDataSource =
+        DDayLocalDataSourceImpl(dDayDao)
 }
