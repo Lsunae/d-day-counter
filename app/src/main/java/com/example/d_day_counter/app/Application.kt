f0bda179f6ad7f7a326fa2e2a1c026c31d4aeae2
Lsunae <sjsao5012@gmail.com>
2025-07-16 11:39:49 +0900

    Refactor: Application 상속

--- app/src/main/java/com/example/d_day_counter/app/Application.kt
+++ app/src/main/java/com/example/d_day_counter/app/Application.kt
@@ -1,7 +1,7 @@
 package com.example.d_day_counter.app
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Application {
-}
+class Application : Application()
